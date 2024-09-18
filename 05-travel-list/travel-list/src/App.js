5b740a1ea05c0b2b021b7af492515ef7c1c225ae
vybukhivka <99396589+vybukhivka@users.noreply.github.com>
2024-09-18 18:21:26 +0300

    derived state

--- 05-travel-list/travel-list/src/App.js
+++ 05-travel-list/travel-list/src/App.js
@@ -24,7 +24,7 @@ function App() {
 				onDeleteItem={handleDeleteItem}
 				onToggleItem={handleToggleItem}
 			/>
-			<Stats />
+			<Stats items={items} />
 		</div>
 	)
 }
@@ -44,7 +44,6 @@ function Form({onAddItems}) {
 		if(description === '') return
 
 		const newItem = {description, quantity, packed: false, id: Date.now()}
-		console.log(newItem)
 		onAddItems(newItem)
 
 		setDescription('')
@@ -75,11 +74,27 @@ function Form({onAddItems}) {
 }
 
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+	const [sortBy, setSortBy] = useState('input')
+
+	let sortedItems	
+
+	if(sortBy === 'input') sortedItems = items
+	if(sortBy === 'description') sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description))
+	if(sortBy === 'packed') sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed))
+
 	return (
 		<div className="list">
 			<ul>
-				{items.map(item => <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id} />)}		
+				{sortedItems.map(item => <Item item={item} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} key={item.id} />)}		
 			</ul>
+
+			<div className="actions">
+				<select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+					<option value='input'>Sort by input order</option>
+					<option value='description'>Sort by description</option>
+					<option value='packed'>Sort by packed status</option>
+				</select>
+			</div>
 		</div>
 	) 
 }
@@ -101,10 +116,15 @@ function Item({item, onDeleteItem, onToggleItem}) {
 	) 
 }
 
-function Stats() {
+function Stats({items}) {
+	if(!items.length) return <p className="stats"><em>Start adding some items to packing list</em></p>
+	const numItems = items.length
+	const numPacked = items.filter(item => item.packed).length
+	const percentagePacked = Math.round((numPacked / numItems) * 100)
+
 	return (
 		<footer className="stats">
-			<em>You have X items on your list, and you already packed X (X%)</em>
+			<em>{percentagePacked === 100 ? 'You got everything ready to go!' : `You have ${numItems} items on your list, and you already packed ${numPacked} (${percentagePacked}%)`}</em>
 		</footer>
 	)	
 }
