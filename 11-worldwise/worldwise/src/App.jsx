6da2e3ff54c0ac69a9548aaaa5c1613dd1e9b0bd
victorb <99396589+vybukhivka@users.noreply.github.com>
2024-10-03 21:01:55 +0300

    router

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -5,6 +5,7 @@ import Pricing from "./pages/Pricing"
 import Homepage from "./pages/Homepage"
 import PageNotFound from "./pages/PageNotFound"
 import AppLayout from "./pages/AppLayout"
+import Login from "./pages/Login"
 
 function App() {
 	return (
@@ -14,7 +15,12 @@ function App() {
 					<Route path='/' element={<Homepage />} />
 					<Route path='product' element={<Product />} />
 					<Route path='pricing' element={<Pricing />} />
-					<Route path='app' element={<AppLayout />} />
+					<Route path='login' element={<Login />} />
+					<Route path='app' element={<AppLayout />}>
+						<Route path='cities' element={<p>List of cities</p>} />
+						<Route path='countries' element={<p>Countries</p>} />
+						<Route path='form' element={<p>Form</p>} />
+					</Route>
 					<Route path='*' element={<PageNotFound />} />
 				</Routes>
 			</BrowserRouter>
