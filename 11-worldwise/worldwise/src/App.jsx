b1a7bab2dad9f628f0bc949c0d364d0b8f622b72
victorb <99396589+vybukhivka@users.noreply.github.com>
2024-10-08 15:56:06 +0300

    router basics finished

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 
 import Product from "./pages/Product"
 import Pricing from "./pages/Pricing"
@@ -10,6 +10,7 @@ import Login from "./pages/Login"
 import CityList from "./components/CityList"
 import CountryList from "./components/CountryList"
 import City from "./components/City"
+import Form from "./components/Form"
 
 const BASE_URL = 'http://localhost:8000'
 
@@ -45,11 +46,11 @@ function App() {
 					<Route path='pricing' element={<Pricing />} />
 					<Route path='login' element={<Login />} />
           <Route path='app' element={<AppLayout />}>
-            <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+            <Route index element={<Navigate replace to='cities' />} />
             <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
             <Route path='cities/:id' element={<City />} />
 						<Route path='countries' element={<CountryList cities={cities} isLoading={isLoading} />} />
-						<Route path='form' element={<p>Form</p>} />
+						<Route path='form' element={<Form />} />
 					</Route>
 					<Route path='*' element={<PageNotFound />} />
 				</Routes>
--- 11-worldwise/worldwise/src/components/CountryList.jsx
+++ 11-worldwise/worldwise/src/components/CountryList.jsx
@@ -17,7 +17,7 @@ function CountryList({cities, isLoading}) {
 
   return (
     <ul className={styles.countryList}>
-      {countries.map((country, i) => <CountryItem country={country} key={i}/> )} 
+      {countries.map(country => <CountryItem country={country} key={country.country}/> )} 
     </ul>
   )
 }
